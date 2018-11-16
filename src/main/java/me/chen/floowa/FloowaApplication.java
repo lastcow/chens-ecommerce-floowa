903fb303cd3f94c8a5407d65a89f0761a61c3918
zhijiang/chen <zhijiang@chen.me>
2018-11-16 17:00:30 -0500

    Project init.

---
+++ src/main/java/me/chen/floowa/FloowaApplication.java
@@ -0,0 +1,12 @@
+package me.chen.floowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloowaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloowaApplication.class, args);
+    }
+}
