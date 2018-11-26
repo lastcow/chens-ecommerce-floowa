d79723276a5892b71c4b3a3321b32a69483c06c9
Zhijiang Chen <zhijiang@chen.me>
2018-11-26 00:19:01 -0500

    disable csrf

--- src/main/java/me/chen/floowa/configuration/WebSecurityConfiguration.java
+++ src/main/java/me/chen/floowa/configuration/WebSecurityConfiguration.java
@@ -37,6 +37,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http
                 .authorizeRequests()
                     .antMatchers("/").permitAll()
